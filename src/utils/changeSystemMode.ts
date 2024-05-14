75dae410e9407afbe216606b9d0f3ced2ce2e0ff
xplore <epfl-xplore@gmail.ch>
2024-05-14 22:44:55 +0200

    start services frontend

--- src/utils/changeSystemMode.ts
+++ src/utils/changeSystemMode.ts
@@ -2,12 +2,13 @@ import { getCookie } from "./requests"
 
 const requestChangeMode = (system: string, mode: any) => {
     const csrftoken = getCookie("csrftoken");
+	console.log("change")
 
     const data = new FormData();
 	data.append("system", system);
     data.append("mode", mode);
 
-    return new Request("http://" + window.location.host + "/csApp/changeModeSystem/" + system + "/", {
+    return new Request("http://" + window.location.host + "/csApp/changeModeSystem/", {
 		method: "POST",
 		body: data,
 		headers: { "X-CSRFToken": csrftoken ?? "" },
