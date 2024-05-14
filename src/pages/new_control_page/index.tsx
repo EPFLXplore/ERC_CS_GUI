b8dac912953f04ce6cbc8986e340711abc2ebab3
xplore <epfl-xplore@gmail.ch>
2024-05-14 21:33:06 +0200

    new request services

--- src/pages/new_control_page/index.tsx
+++ src/pages/new_control_page/index.tsx
@@ -15,6 +15,8 @@ import SystemMode from "../../components/SystemMode";
 import Simulation from "../../components/Simulation";
 import RoverData from "../../components/RoverData";
 
+import requestChangeMode from "../../utils/changeSystemMode";
+
 import logo from "../../assets/images/logos/logo_XPlore.png";
 
 export default () => {
@@ -87,25 +89,25 @@ export default () => {
 						system="Navigation"
 						currentMode="Manual"
 						modes={["Auto", "Manual", "Off"]}
-						onSelect={(mode) => console.log(mode)}
+						onSelect={(mode) => requestChangeMode("navigation", mode)}
 					/>
 					<SystemMode
 						system="Handling Device"
 						currentMode="Off"
 						modes={["Auto", "Manual", "Off"]}
-						onSelect={(mode) => console.log(mode)}
+						onSelect={(mode) => requestChangeMode("handling_device", mode)}
 					/>
 					<SystemMode
 						system="Cameras"
 						currentMode="Stream"
 						modes={["Stream", "Off"]}
-						onSelect={(mode) => console.log(mode)}
+						onSelect={(mode) => requestChangeMode("cameras", mode)}
 					/>
 					<SystemMode
 						system="Drill"
 						currentMode="Off"
 						modes={["On", "Off"]}
-						onSelect={(mode) => console.log(mode)}
+						onSelect={(mode) => requestChangeMode("drill", mode)}
 					/>
 				</div>
 				<Timer end={Date.now() + 10000} size={Size.SMALL} />
